22f4b2790bb6e3ab48c1bc4041d8d19b945eb4ea
Hikoyann <i21i022dl@ous.jp>
2024-10-04 16:26:27 +0900

    備品レンタルフォームの場所を変更

--- src/pages/equipmentRegistry/[id]/index.jsx
+++ src/pages/equipmentRegistry/[id]/index.jsx
@@ -1,4 +1,11 @@
 import Head from "next/head";
+import React, { useCallback, useEffect, useState } from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, database, provider } from "../../../lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { ref, get, set, push } from "firebase/database";
+// import { QRCode } from 'qrcode.react';
+import QRCodeLib from "qrcode";
 
 export const getServerSideProps = async (ctx) => {
   const { id } = ctx.query;
@@ -20,18 +27,129 @@ const ID = ({ mgmt }) => {
     return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
+  const [user] = useAuthState(auth);
+
+  const [inputs, setInputs] = useState({
+    num: "",
+    name: "",
+    equipment: "",
+    purpose: "",
+    returnDate: "",
+  });
+
+  const [form, setForm] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (user) {
+      // ログイン状態を確認
+      const equipmentRef = ref(database, "equipments");
+      const newEquipmentRef = push(equipmentRef);
+
+      const updatedInputs = {
+        ...inputs,
+        email: user.email,
+      };
+
+      // データベースに送信
+      set(newEquipmentRef, updatedInputs);
+      setForm([...form, updatedInputs]);
+      setInputs({
+        num: "",
+        name: "",
+        equipment: "",
+        purpose: "",
+        returnDate: "",
+      });
+    }
+  };
+
   return (
     <>
       <Head>
         <title>{mgmt.equipmentName} - 備品レンタル情報</title>
       </Head>
+      <div className="bg-gray-300 min-h-screen flex flex-col items-center">
+        <div>
+          <h1>備品情報</h1>
+          <div>番号: {mgmt.num}</div>
+          <div>備品名: {mgmt.equipmentName}</div>
+          <div>使用用途: {mgmt.equipmentDetails}</div>
+          <div>登録した日付: {formatDate(mgmt.addedDate)}</div>
+        </div>
 
-      <div>
-        <h1>備品情報</h1>
-        <div>番号: {mgmt.num}</div>
-        <div>備品名: {mgmt.equipmentName}</div>
-        <div>使用用途: {mgmt.equipmentDetails}</div>
-        <div>登録した日付: {formatDate(mgmt.addedDate)}</div>
+        <div className="w-full">
+          {user ? (
+            <div className="mt-4">
+              <h2 className="text-lg font-semibold">備品レンタルフォーム</h2>
+              <form onSubmit={handleSubmit} className="mt-2">
+                <div className="mb-4">
+                  <label className="block">番号</label>
+                  <input
+                    type="number"
+                    name="num"
+                    value={inputs.num}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block">借りる人の名前</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={inputs.name}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block">備品名</label>
+                  <input
+                    type="text"
+                    name="equipment"
+                    value={inputs.equipment}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block">使用用途</label>
+                  <input
+                    type="text"
+                    name="purpose"
+                    value={inputs.purpose}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block">返却予定日</label>
+                  <input
+                    type="date"
+                    name="returnDate"
+                    value={inputs.returnDate}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="inline-block bg-slate-500 hover:bg-slate-700 text-white font-bold py-1 px-2 rounded-sm shadow-lg hover:shadow-xl transform hover:scale-105 transition-transform duration-200 ease-in-out mt-4">
+                  <button type="submit">送信</button>
+                </div>
+              </form>
+            </div>
+          ) : (
+            <div className="mb-4">
+              レンタルフォームを使用するには、サインインしてください。
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
--- src/pages/equipmentRegistry/index.jsx
+++ src/pages/equipmentRegistry/index.jsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
+import React, { useCallback, useEffect, useState } from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, database, provider } from "../../lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { ref, get, set, push } from "firebase/database";
+// import { QRCode } from 'qrcode.react';
+import QRCodeLib from "qrcode";
 
 const Posts = () => {
   return (
     <>
       <Head>
-        <title>AAA Page</title>
+        <title>備品一覧 Page</title>
       </Head>
 
       <div>
